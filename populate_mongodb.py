9b25d072c4d95a41a81825773ef87832e6d93878
yevano09 <jawahar.m@gmail.com>
2018-10-15 15:30:56 +0530

    changed time to refresh

--- populate_mongodb.py
+++ populate_mongodb.py
@@ -11,8 +11,8 @@ client = mqtt.Client("P1")
 client.connect(broker_address)
 
 
-for x in xrange(1,10):
-	rd = random.randrange(0, 101, 2)
+for x in xrange(1,20):
+	rd = random.randrange(0, 200, 2)
 	msg = "{ \"id\": \"test1\", \"temp\": %d }" % rd 
 	client.publish("/iot/home/2", msg)
 	print(" Sent the meessage %s \n " % msg )
